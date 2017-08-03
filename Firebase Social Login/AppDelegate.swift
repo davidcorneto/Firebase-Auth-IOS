faba8a7c169909085347b7898058f300964b2445
David Cornelius <davidcornelius15@gmail.com>
2017-08-03 15:15:57 +0700

    FIx add Firebase/Auth with custom button and facebook sdk

--- Firebase Social Login/AppDelegate.swift
+++ Firebase Social Login/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import FBSDKCoreKit
-
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
  
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FIRApp.configure()
+        
         
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
